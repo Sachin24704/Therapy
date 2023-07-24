83cf4fcd1544aa1d815474e174150a232e19c70a
Sachin24704 <sachin24704@gmail.com>
2023-07-24 13:18:24 +0530

    signinwithgogle function

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,17 +2,45 @@
 
 import { useEffect, useState } from "react";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { auth } from "@/firebase/firebase";
+import {
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setpassword] = useState("");
+  const router = useRouter();
+  const provider = GoogleAuthProvider();
   const handleChange = (e) => {
     if (e.target.name === "email") setEmail(e.target.value);
     else setpassword(e.target.value);
   };
-  const handleSubmit = () => {
-    console.log(hi);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("hi");
+    if (!email || !password) return;
+    try {
+      const user = await createUserWithEmailAndPassword(auth, email, password);
+      console.log(user);
+    } catch (error) {
+      console.log("error", error);
+    }
+    // TODO: implement authentication
+
     router.push("/signup");
+  };
+  const signInWithGoogle = async () => {
+    try {
+      // let result =await auth().signInWithPopup(provider).then((res)=>console.log(res));
+      const user = await signInWithPopup(auth, provider);
+      console.log(user);
+    } catch (error) {
+      console.log("error", error);
+    }
   };
   return (
     <>
