9900450c2cdeb806fdfe018d156edc6940266352
Sachin24704 <sachin24704@gmail.com>
2023-08-28 11:50:58 +0530

    chatbot

---
+++ src/app/chatbot/page.js
@@ -0,0 +1,12 @@
+import Chat from "@/components/Chat";
+const chatbot = () => {
+  return (
+    <>
+      <div className="chat-container">
+        <Chat />
+      </div>
+    </>
+  );
+};
+
+export default chatbot;
---
+++ src/components/Chat.jsx
@@ -0,0 +1,16 @@
+export default function Chat({ props }) {
+  return (
+    <>
+      <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl justify-normal break-words  ">
+        <div className="p-2 text-white text-left  ">
+          asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshghjhhjhnnh
+        </div>
+      </div>
+      <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl ml-auto break-words  ">
+        <div className="p-2 text-white text-left  ">
+          asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshghjhhjhnnh
+        </div>
+      </div>
+    </>
+  );
+}
