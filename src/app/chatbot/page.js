d3081183981a3b09d621675a411bd24faaf762af
Sachin24704 <sachin24704@gmail.com>
2023-08-28 12:39:18 +0530

    chat

--- src/app/chatbot/page.js
+++ src/app/chatbot/page.js
@@ -1,11 +1,39 @@
+"use client";
+import react from "react";
+import { useState, useEffect } from "react";
 import Chat from "@/components/Chat";
 const chatbot = () => {
+  const [msg, setMsg] = useState("");
+  const [chat, setChat] = useState([
+    {
+      role: "assisant",
+      content: "Hi...how are you? Hope you are taking care of your feelings",
+    },
+  ]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setChat([...chat, { role: "user", content: msg }]);
+    setMsg(""); // value displayed in input box
+  };
   return (
-    <>
+    <div className="flex-col bg-slate-600 min-h-screen ">
       <div className="chat-container">
-        <Chat />
+        {chat.map((chat, index) => {
+          return <Chat key={index} props={chat} />;
+        })}
+        {/* <Chat /> */}
       </div>
-    </>
+      <div className="w-fit">
+        <form className="w-full" onSubmit={handleSubmit}>
+          <input
+            className=""
+            type=""
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+        </form>
+      </div>
+    </div>
   );
 };
 
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,16 +1,17 @@
 export default function Chat({ props }) {
   return (
     <>
-      <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl justify-normal break-words  ">
-        <div className="p-2 text-white text-left  ">
-          asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshghjhhjhnnh
+      {props.role === "user" ? (
+        <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl ml-auto break-words  ">
+          <div className="p-2 text-white text-left  ">user-{props.content}</div>
         </div>
-      </div>
-      <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl ml-auto break-words  ">
-        <div className="p-2 text-white text-left  ">
-          asssssssssssssssssssssssssssssssssssssssssssssssssssssssssssshghjhhjhnnh
+      ) : (
+        <div className=" container box-border max-w-lg bg-blue-500 p-1 m-2 rounded-2xl justify-normal break-words  ">
+          <div className="p-2 text-white text-left  ">
+            assistant-{props.content}
+          </div>
         </div>
-      </div>
+      )}
     </>
   );
 }
