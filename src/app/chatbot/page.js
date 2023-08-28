3089580bd45359a01edccb3eed9ec4fb0271bdbd
Sachin24704 <sachin24704@gmail.com>
2023-08-28 12:47:36 +0530

    inout design change

--- src/app/chatbot/page.js
+++ src/app/chatbot/page.js
@@ -23,7 +23,7 @@ const chatbot = () => {
         })}
         {/* <Chat /> */}
       </div>
-      <div className="w-fit">
+      {/* <div className="p-2 mb-0 fixed">
         <form className="w-full" onSubmit={handleSubmit}>
           <input
             className=""
@@ -31,6 +31,23 @@ const chatbot = () => {
             value={msg}
             onChange={(e) => setMsg(e.target.value)}
           />
+        </form>
+      </div> */}
+      <div className="fixed bottom-0 left-0 w-full bg-white p-4 shadow-md">
+        <form className="flex items-center" onSubmit={handleSubmit}>
+          <input
+            className="flex-1 bg-gray-100 rounded-full px-4 py-2 focus:outline-none"
+            type="text"
+            placeholder="Type your message..."
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+          <button
+            className="ml-4 bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-full"
+            type="submit"
+          >
+            Send
+          </button>
         </form>
       </div>
     </div>
