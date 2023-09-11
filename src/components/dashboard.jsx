ef63eb3795c2f43265237521630d872f9713ea8c
Sachin24704 <sachin24704@gmail.com>
2023-09-11 13:47:51 +0530

    dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -24,36 +24,38 @@
 // }
 
 import Link from "next/link";
-import { useState } from "react";
 import { useAuth } from "@/firebase/auth";
-
-import Loader from "@/components/loader";
 
 export default function Dashboard({ isHome }) {
   const { isLoading, authUser, signout } = useAuth();
 
   return (
-    <div className="flex bg-blue-200  ">
-      <div className="flex flex-row justify-between items-center mb-4">
-        <span className="mx- font-semibold">Welcome, {authUser?.username}</span>
-        {isHome ? (
-          <div className="">
-            <Link href="/chatbot">Chatbot</Link>
-          </div>
-        ) : (
-          <div className="">
-            <Link href="/home">Home</Link>
-          </div>
-        )}
-
-        <button
-          className="bg-white-500 text-black ml-0 hover:bg-red-600 text-white px-4 py-2 rounded"
-          onClick={signout}
-        >
-          Sign Out
-        </button>
+    <div className="bg-blue-200 py-2 px-4">
+      <div className="container mx-auto flex flex-row justify-between items-center">
+        <div className="flex items-center space-x-4">
+          <span className="text-xl font-semibold text-blue-900">Therapy</span>
+          <span className="text-lg font-semibold text-blue-900 ml-4">
+            Welcome, {authUser?.username}
+          </span>
+        </div>
+        <div className="space-x-4">
+          {isHome ? (
+            <Link className="text-blue-500 hover:underline" href="/chatbot">
+              Chatbot
+            </Link>
+          ) : (
+            <Link className="text-blue-500 hover:underline" href="/home">
+              Home
+            </Link>
+          )}
+          <button
+            className="bg-blue-500 hover:bg-red-600 text-white px-4 py-2 rounded"
+            onClick={signout}
+          >
+            Sign Out
+          </button>
+        </div>
       </div>
-      {/* Add other content */}
     </div>
   );
 }
