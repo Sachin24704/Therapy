636f1eb84ca398344179fd2765a00e155a8e98a6
Sachin24704 <sachin24704@gmail.com>
2024-02-16 14:01:20 +0530

    dashboard made responsive for mobile

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -41,38 +41,110 @@ export default function Dashboard({ isHome, isTherapy, bgColour }) {
             </button>
           )}
         </div>
-        <button
-          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-          onClick={toggleMobileMenu}
-          aria-controls="navbar-default"
-          aria-expanded={isMobileMenuOpen ? "true" : "false"}
-        >
-          <span className="sr-only">
-            {isMobileMenuOpen ? "Close" : "Open main menu"}
-          </span>
-          <svg
-            className="w-5 h-5"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 17 14"
-          >
-            <path
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M1 1h15M1 7h15M1 13h15"
-            />
-          </svg>
-        </button>
-        {/* < div className="hidden lg:flex space-x-4  flex-row items-center"> */}
-        <div
-          className={`lg:flex flex-col lg:flex-row items-center space-y-4 lg:space-y-0 ${
-            isMobileMenuOpen ? "block" : "hidden"
-          }`}
-          id="navbar-default"
-        >
+        <div className="lg:hidden">
+          <div className="relative">
+            <button
+              className="inline-flex items-center  p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              onClick={toggleMobileMenu}
+              aria-controls="navbar-default"
+              aria-expanded={isMobileMenuOpen ? "true" : "false"}
+            >
+              <span className="sr-only">
+                {isMobileMenuOpen ? "Close" : "Open main menu"}
+              </span>
+              <svg
+                className="w-5 h-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            {/* < div className="hidden lg:flex space-x-4  flex-row items-center"> */}
+            <div
+              className={`absolute right-0 bg-gray-100 ${
+                isMobileMenuOpen ? "block" : "hidden"
+              } `}
+              id="navbar-default"
+            >
+              {isHome ? (
+                <div>
+                  <Link
+                    className="block lg:inline-block p-4 text-blue-500 hover:underline font-bold"
+                    href="/chatbot"
+                  >
+                    Chatbot
+                  </Link>
+                  {/* <Link
+                className=" p-4  text-blue-500 hover:underline font-bold"
+                href="/about"
+              >
+                About
+              </Link> */}
+                  <Link
+                    className="block lg:inline-block p-4 text-blue-500 hover:underline font-bold"
+                    href="/View"
+                  >
+                    Therapist
+                  </Link>
+                </div>
+              ) : isTherapy ? (
+                <div>
+                  <Link
+                    className="p-4 text-blue-500 hover:underline font-bold"
+                    href="/home"
+                  >
+                    Home
+                  </Link>
+                  {/* <Link
+                className="p-4 text-blue-500 hover:underline font-bold"
+                href="/chatbot"
+              >
+                About
+              </Link> */}
+                  <Link
+                    className="p-4 text-blue-500 hover:underline font-bold"
+                    href="/chatbot"
+                  >
+                    Chatbot
+                  </Link>
+                </div>
+              ) : (
+                <div>
+                  <Link
+                    className="p-4 text-blue-500 hover:underline font-bold"
+                    href="/home"
+                  >
+                    Home
+                  </Link>
+                  {/* <Link
+                className="p-4 text-blue-500 hover:underline font-bold"
+                href="/chatbot"
+              >
+                About
+              </Link> */}
+                  <Link
+                    className="p-4 text-blue-500 hover:underline font-bold"
+                    href="/View"
+                  >
+                    Therapist
+                  </Link>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        {/* signout button at end when screen is large and navlar li when screen is large */}
+        {/* <div className="hidden flex-row  lg:block"> */}
+        <div className="hidden  lg:flex space-x-4  flex-row items-center">
           {isHome ? (
             <div>
               <Link
@@ -138,8 +210,7 @@ export default function Dashboard({ isHome, isTherapy, bgColour }) {
             </div>
           )}
         </div>
-        {/* signout button at start when screen is large */}
-        <div className="hidden lg:block">
+        <div className=" hidden lg:block">
           {authUser ? (
             <button
               className="bg-blue-500 hover:bg-red-600 text-white px-4 py-2 rounded"
@@ -153,6 +224,7 @@ export default function Dashboard({ isHome, isTherapy, bgColour }) {
             </button>
           )}
         </div>
+        {/* </div> */}
       </div>
     </nav>
   );
